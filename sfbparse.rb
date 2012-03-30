2228a3a231966fad7c516735a885346c4961b3bb
Clay Shentrup <clay@brokenladder.com>
2012-03-30 12:37:22 -0700

    added fixture data and basic parsing of candidate list

--- sfbparse.rb
+++ sfbparse.rb
@@ -1,2 +1,38 @@
 class SFBallotParse
+  attr_reader :candidates
+
+  def initialize(source_dir_relative_path)
+    @source_dir_relative_path = source_dir_relative_path
+    set_candidates
+  end
+  
+  private
+  
+  def set_candidates
+    @candidates = read_candidates
+  end
+  
+  def read_candidates
+    File.open(master_lookup) do |file|
+      file.map do |line|
+        line[17,50].rstrip
+      end
+    end
+  end
+
+  def this_dir
+    File.dirname(__FILE__)
+  end
+
+  def source_dir_path
+    File.join(this_dir, @source_dir_relative_path)
+  end
+
+  def master_lookup_glob
+    File.join(source_dir_path, '*-MasterLookup.txt')
+  end
+    
+  def master_lookup
+    Dir.glob(master_lookup_glob).first
+  end
 end
--- sfbparse_spec.rb
+++ sfbparse_spec.rb
@@ -2,7 +2,13 @@ require 'rspec'
 require './sfbparse'
 
 describe SFBallotParse do
+  subject { SFBallotParse.new('spec/fixtures/2012-mayor') }
+
   it 'should work' do
-    
+    subject
+  end
+  
+  describe '#candidates' do
+    its(:candidates) { should include 'LELAND YEE' }
   end
 end
