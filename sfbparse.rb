e80e5b168f7a4d0147a62ae46d492a73c6d9e5ef
Clay Shentrup <clay@brokenladder.com>
2012-03-30 13:12:47 -0700

    only treat a master lookup line as a candidate if it begins with 'Candidate'

--- sfbparse.rb
+++ sfbparse.rb
@@ -11,12 +11,14 @@ class SFBallotParse
   def set_candidates
     @candidates = read_candidates
   end
-  
+
   def read_candidates
     File.open(master_lookup) do |file|
-      file.map do |line|
-        line[17,50].rstrip
+      result = []
+      while (line = MasterLookupLine.new(file.gets)) && line.is_candidate?
+        result << line.candidate_name
       end
+      result
     end
   end
 
@@ -34,5 +36,19 @@ class SFBallotParse
     
   def master_lookup
     Dir.glob(master_lookup_glob).first
+  end
+end
+
+class MasterLookupLine
+  def initialize(line)
+    @line = line
+  end
+  
+  def candidate_name
+    @line[17,50].rstrip
+  end
+  
+  def is_candidate?
+    !!@line.match(/^Candidate/)
   end
 end
--- sfbparse_spec.rb
+++ sfbparse_spec.rb
@@ -9,6 +9,16 @@ describe SFBallotParse do
   end
   
   describe '#candidates' do
-    its(:candidates) { should include 'LELAND YEE' }
+    it 'gets the first candidate' do
+      subject.candidates.should include 'LELAND YEE'
+    end
+    
+    it 'gets the last candidate' do
+      subject.candidates.should include 'WRITE-IN DAVID VILLA-LOBOS'
+    end
+    
+    it 'processes all candidates, and no other record types' do
+      subject.candidates.length.should == 25
+    end
   end
 end
